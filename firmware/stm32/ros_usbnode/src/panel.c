ac919cb41aa83e61f45870e7546bbf168b2b476f
Bruno Hjorth <bruno@MacBook-Pro-som-tillhor-Bruno.local>
2026-09-19 14:44:45 +0200

    fix(firmware): configure F401 panel RX as alternate function

--- firmware/stm32/ros_usbnode/src/panel.c
+++ firmware/stm32/ros_usbnode/src/panel.c
@@ -96,7 +96,11 @@ void PANEL_Init(void)
 
     // RX
     GPIO_InitStruct.Pin = PANEL_USART_RX_PIN;
+#if BOARD_YARDFORCE500_VARIANT_B
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+#else
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+#endif
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
 #if BOARD_YARDFORCE500_VARIANT_B
